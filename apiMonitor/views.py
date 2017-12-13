a2c84f07ab05b18a60e85d90fa3bcd33b0ed2827
root <slc14211421@163.com>
2017-12-13 09:21:52 +0800

    add apiMonitor

---
+++ apiMonitor/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import request,HttpResponse
+import requests
+from . import checkAction
+
+# Create your views here.
+
+def checkApi(request):
+    if request.method == 'GET':
+
+        projectNname =request.GET.get('project')
+        hostip=request.GET.get('hostip')
+        if projectNname == "market" :
+            result=checkAction.checkMarket(hostip)
+
+        return HttpResponse(result)
+    else:
+        return HttpResponse("This is Juest for HTTP GET")
+
+
+
