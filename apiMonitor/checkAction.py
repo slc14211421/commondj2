47635cd6e24ea93a41789afcf66cfc56b86d73e6
root <slc14211421@163.com>
2017-12-13 17:16:13 +0800

    add apiMonitor

--- apiMonitor/checkAction.py
+++ apiMonitor/checkAction.py
@@ -51,6 +51,37 @@ def checkPortalProxy(hostip):
     except:
         return "FAILED"
 
+def checkliuhuaPortal(hostip):
+    port = "8070"
+    suburl1 = "/portal/interface/video/list?categoryId=567720&pageSize=12&pageNum=1"
+    suburl2 = "/portal/api/watch/listRoller?stbNo=01486617150001110&appClientType=00003"
+    suburl3 = "/portal/api/video/price?stbNo=99586611350156907&id=567720_2429124OOOOOOOOOOOO3"
+
+    checkurl1 = "http://" + hostip + ":" + port + suburl1
+    checkurl2 = "http://" + hostip + ":" + port + suburl2
+    checkurl3 = "http://" + hostip + ":" + port + suburl3
+    checkUrls = [checkurl1, checkurl2, checkurl3]
+    flag = 1
+
+    try:
+        for checkurl in checkUrls:
+            res = requests.get(url=checkurl, timeout=5)
+            if res.status_code == 200:
+                resdic = simplejson.JSONDecoder( ).decode(res.content)
+                if resdic['status'] != "200":
+                    flag = 0
+                    break
+            else:
+                flag = 0
+                break
+        if flag == 1:
+            return "SUCESS"
+        else:
+            return "FAILED"
+    except:
+        return "FAILED"
+
+
 
 
 
